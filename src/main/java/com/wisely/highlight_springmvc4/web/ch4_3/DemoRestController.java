25d85299ca95bacdda6024bae165abec94b474ec
nbaisino\guoxingzhang <nbaisino@123.com>
2017-05-02 13:52:50 +0800

    Initial commit

---
+++ src/main/java/com/wisely/highlight_springmvc4/web/ch4_3/DemoRestController.java
@@ -0,0 +1,24 @@
+package com.wisely.highlight_springmvc4.web.ch4_3;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wisely.highlight_springmvc4.domain.DemoObj;
+
+@RestController //1
+@RequestMapping("/rest")
+public class DemoRestController {
+	
+	@RequestMapping(value = "/getjson",
+			produces={"application/json;charset=UTF-8"}) //2
+	public DemoObj getjson (DemoObj obj){
+		return new DemoObj(obj.getId()+1, obj.getName()+"yy");//3
+	}
+	@RequestMapping(value = "/getxml",
+			produces={"application/xml;charset=UTF-8"})//4
+	public DemoObj getxml(@ModelAttribute("demoObj") DemoObj obj){
+		return new DemoObj(obj.getId()+1, obj.getName()+"yy");
+	}
+
+}
